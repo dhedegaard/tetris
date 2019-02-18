20bb21026e069eedd4ff1a00d9ee47933db3a4a7
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-02-18 21:32:51 +0100

    Added an initial grid.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Grid from './components/Grid'
 import { Direction } from './components/shapes'
 import I from './components/shapes/I'
 import J from './components/shapes/J'
@@ -11,27 +12,27 @@ import Z from './components/shapes/Z'
 class App extends Component {
   render() {
     return (
-      <svg width="600" height="400" style={{ backgroundColor: '#111' }}>
-        <I x={2} y={2} direction={Direction.UP} />
-        <I x={5} y={2} direction={Direction.LEFT} />
-        <O x={9} y={2} direction={Direction.UP} />
-        <T x={13} y={2} direction={Direction.UP} />
+      <Grid>
+        <I x={1} y={2} direction={Direction.UP} />
+        <I x={4} y={1} direction={Direction.LEFT} />
+        <O x={3} y={3} direction={Direction.UP} />
+        <T x={7} y={4} direction={Direction.UP} />
         <T x={16} y={2} direction={Direction.RIGHT} />
         <T x={2} y={6} direction={Direction.DOWN} />
         <T x={6} y={6} direction={Direction.LEFT} />
-        <S x={8} y={6} direction={Direction.UP} />
-        <S x={12} y={6} direction={Direction.LEFT} />
-        <Z x={15} y={6} direction={Direction.UP} />
-        <Z x={19} y={6} direction={Direction.LEFT} />
-        <J x={20} y={2} direction={Direction.UP} />
-        <J x={23} y={2} direction={Direction.DOWN} />
-        <J x={26} y={2} direction={Direction.LEFT} />
+        <S x={7} y={2} direction={Direction.UP} />
+        <S x={2} y={16} direction={Direction.LEFT} />
+        <Z x={3} y={17} direction={Direction.UP} />
+        <Z x={7} y={17} direction={Direction.LEFT} />
+        <J x={2} y={14} direction={Direction.UP} />
+        <J x={5} y={14} direction={Direction.DOWN} />
+        <J x={8} y={13} direction={Direction.LEFT} />
         <J x={23} y={7} direction={Direction.RIGHT} />
         <L x={2} y={10} direction={Direction.UP} />
         <L x={5} y={10} direction={Direction.DOWN} />
         <L x={8} y={10} direction={Direction.LEFT} />
         <L x={12} y={10} direction={Direction.RIGHT} />
-      </svg>
+      </Grid>
     )
   }
 }
---
+++ src/components/Grid.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const HEIGHT = 20
+const WIDTH = 10
+const COLOR = '#efefef'
+
+const Grid: React.FunctionComponent = props => {
+  const lines = []
+  for (let x = 20; x < WIDTH * 20; x += 20) {
+    lines.push(
+      <line
+        key={`grid_x_${x}`}
+        x1={x}
+        x2={x}
+        y1={0}
+        y2={HEIGHT * 20}
+        stroke={COLOR}
+      />
+    )
+  }
+  for (let y = 20; y < HEIGHT * 20; y += 20) {
+    lines.push(
+      <line
+        key={`grid_y_${y}`}
+        x1={0}
+        x2={WIDTH * 20}
+        y1={y}
+        y2={y}
+        stroke={COLOR}
+      />
+    )
+  }
+  return (
+    <svg
+      width={WIDTH * 20}
+      height={HEIGHT * 20}
+      style={{ backgroundColor: '#333' }}
+    >
+      {lines}
+      {props.children}
+    </svg>
+  )
+}
+
+export default Grid
