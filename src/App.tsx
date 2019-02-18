7c7da71cbf2fe4f5f1d042fb7eabecfb697b0cef
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-02-18 21:49:50 +0100

    Initial hooks for making something work.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,42 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import Grid from './components/Grid'
-import { Direction } from './components/shapes'
-import I from './components/shapes/I'
-import J from './components/shapes/J'
-import L from './components/shapes/L'
-import O from './components/shapes/O'
-import S from './components/shapes/S'
-import T from './components/shapes/T'
-import Z from './components/shapes/Z'
+import { Direction, nextDirection, Shape, Shapes } from './components/shapes'
 
-const App: React.FunctionComponent = props => {
+const App: React.FunctionComponent = () => {
+  const [position, setPosition] = useState({ x: 5, y: 1 })
+  const [shape, setShape] = useState<Shapes>('J')
+  const [direction, setDirection] = useState(Direction.UP)
+
+  useEffect(() => {
+    const keypressHandler = (evt: KeyboardEvent) => {
+      switch (evt.keyCode) {
+        case 37: // left
+          setPosition({ ...position, x: Math.max(position.x - 1, 0) })
+          break
+        case 39: // right
+          setPosition({ ...position, x: Math.min(position.x + 1, 19) })
+          break
+        case 38: // up
+          setDirection(nextDirection(direction))
+          break
+        default:
+          console.log('Unknown keycode:', evt.keyCode)
+      }
+    }
+
+    document.addEventListener('keydown', keypressHandler)
+    return () => document.removeEventListener('keydown', keypressHandler)
+  }, [])
+
+  console.log('!!! RENDER')
   return (
     <Grid>
-      <I x={1} y={2} direction={Direction.UP} />
-      <I x={4} y={1} direction={Direction.LEFT} />
-      <O x={3} y={3} direction={Direction.UP} />
-      <T x={7} y={4} direction={Direction.UP} />
-      <T x={16} y={2} direction={Direction.RIGHT} />
-      <T x={2} y={6} direction={Direction.DOWN} />
-      <T x={6} y={6} direction={Direction.LEFT} />
-      <S x={7} y={2} direction={Direction.UP} />
-      <S x={2} y={16} direction={Direction.LEFT} />
-      <Z x={3} y={17} direction={Direction.UP} />
-      <Z x={7} y={17} direction={Direction.LEFT} />
-      <J x={2} y={14} direction={Direction.UP} />
-      <J x={5} y={14} direction={Direction.DOWN} />
-      <J x={8} y={13} direction={Direction.LEFT} />
-      <J x={23} y={7} direction={Direction.RIGHT} />
-      <L x={2} y={10} direction={Direction.UP} />
-      <L x={5} y={10} direction={Direction.DOWN} />
-      <L x={8} y={10} direction={Direction.LEFT} />
-      <L x={12} y={10} direction={Direction.RIGHT} />
+      <Shape
+        direction={direction}
+        shape={shape}
+        x={position.x}
+        y={position.y}
+      />
     </Grid>
   )
 }
--- src/components/shapes/index.tsx
+++ src/components/shapes/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import J from './J'
 import L from './L'
 import O from './O'
