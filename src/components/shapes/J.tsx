91fad3a2df46f2814812edbbd8f257b4811347a5
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-02-19 21:36:14 +0100

    Fix for J shape not being correct.

--- src/components/shapes/J.tsx
+++ src/components/shapes/J.tsx
@@ -22,7 +22,7 @@ export default ({ direction, x, y }: ShapeProps): Coordinates => {
     case Direction.DOWN:
       return [
         { x, y: y - 1 },
-        { x: x - 1, y: y - 1 },
+        { x: x + 1, y: y - 1 },
         { x, y },
         { x, y: y + 1 }
       ]
