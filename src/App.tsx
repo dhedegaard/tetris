0f7aad797a8e10a3342c11b6f96f9759d41e941d
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-02-19 21:08:05 +0100

    Moved all the hooks to separate tetris hook.

--- src/App.tsx
+++ src/App.tsx
@@ -10,79 +10,10 @@ import {
   Shape,
   Shapes
 } from './components/shapes'
+import useTetris from './hooks'
 
 const App: React.FunctionComponent = () => {
-  const [position, setPosition] = useState({ x: 5, y: 1 })
-  const [shape, setShape] = useState<Shapes>(getRandomShape())
-  const [direction, setDirection] = useState(Direction.UP)
-  const [blocks, setBlocks] = useState<Coordinates>([])
-
-  useEffect(() => {
-    const keypressHandler = (evt: KeyboardEvent) => {
-      switch (evt.keyCode) {
-        case 37: // left
-          setPosition(oldPosition => ({
-            ...oldPosition,
-            x: Math.max(oldPosition.x - 1, 1)
-          }))
-          break
-        case 39: // right
-          setPosition(oldPosition => ({
-            ...oldPosition,
-            x: Math.min(oldPosition.x + 1, 20)
-          }))
-          break
-        case 38: // up
-          setDirection(oldDirection => nextDirection(oldDirection))
-          break
-        default:
-          console.log('Unknown keycode:', evt.keyCode)
-      }
-    }
-
-    document.addEventListener('keydown', keypressHandler)
-    return () => document.removeEventListener('keydown', keypressHandler)
-  }, [])
-
-  useEffect(() => {
-    const handle = setInterval(() => {
-      setPosition(oldPosition => {
-        const newY = Math.min(oldPosition.y + 1, 20)
-
-        const newPositions = calculateCoordinates(shape, {
-          direction,
-          x: oldPosition.x,
-          y: newY
-        })
-
-        if (
-          newPositions.find(
-            e =>
-              e.y >= 20 || blocks.find(b => b.x === e.x && b.y === e.y) != null
-          ) != null
-        ) {
-          const oldPositions = calculateCoordinates(shape, {
-            direction,
-            x: oldPosition.x,
-            y: oldPosition.y
-          })
-          setShape(getRandomShape())
-          setDirection(Direction.UP)
-          setBlocks([...blocks, ...oldPositions])
-          return {
-            x: 5,
-            y: 1
-          }
-        }
-
-        return {
-          ...oldPosition,
-          y: newY
-        }
-      })
-    }, 1000)
-    return () => clearInterval(handle)
-  }, [])
+  const { direction, shape, position, blocks } = useTetris()
 
   return (
     <Grid>
---
+++ src/hooks/index.ts
@@ -0,0 +1,92 @@
+import { useEffect, useState } from 'react'
+import { Coordinates } from '../components/ShapeDrawer'
+import {
+  calculateCoordinates,
+  Direction,
+  getRandomShape,
+  nextDirection,
+  Shapes
+} from '../components/shapes'
+
+const useTetris = () => {
+  const [position, setPosition] = useState({ x: 5, y: 1 })
+  const [shape, setShape] = useState<Shapes>(getRandomShape())
+  const [direction, setDirection] = useState(Direction.UP)
+  const [blocks, setBlocks] = useState<Coordinates>([])
+
+  useEffect(() => {
+    const keypressHandler = (evt: KeyboardEvent) => {
+      switch (evt.keyCode) {
+        case 37: // left
+          setPosition(oldPosition => ({
+            ...oldPosition,
+            x: Math.max(oldPosition.x - 1, 1)
+          }))
+          break
+        case 39: // right
+          setPosition(oldPosition => ({
+            ...oldPosition,
+            x: Math.min(oldPosition.x + 1, 20)
+          }))
+          break
+        case 38: // up
+          setDirection(oldDirection => nextDirection(oldDirection))
+          break
+        default:
+          console.log('Unknown keycode:', evt.keyCode)
+      }
+    }
+
+    document.addEventListener('keydown', keypressHandler)
+    return () => document.removeEventListener('keydown', keypressHandler)
+  }, [])
+
+  useEffect(() => {
+    const handle = setInterval(() => {
+      setPosition(oldPosition => {
+        const newY = Math.min(oldPosition.y + 1, 20)
+
+        const newPositions = calculateCoordinates(shape, {
+          direction,
+          x: oldPosition.x,
+          y: newY
+        })
+
+        if (
+          newPositions.find(
+            e =>
+              e.y >= 20 || blocks.find(b => b.x === e.x && b.y === e.y) != null
+          ) != null
+        ) {
+          const oldPositions = calculateCoordinates(shape, {
+            direction,
+            x: oldPosition.x,
+            y: oldPosition.y
+          })
+          setShape(getRandomShape())
+          setDirection(Direction.UP)
+          setBlocks([...blocks, ...oldPositions])
+          return {
+            x: 5,
+            y: 1
+          }
+        }
+
+        return {
+          ...oldPosition,
+          y: newY
+        }
+      })
+    }, 1000)
+    return () => clearInterval(handle)
+  }, [])
+
+  return {
+    direction,
+    shape,
+    position,
+    blocks
+  }
+}
+
+export default useTetris
