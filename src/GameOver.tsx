acb955b14ea152ce041031795f2a1d11429d97bd
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-02-23 16:37:06 +0100

    Improvements to GameOver

--- src/GameOver.tsx
+++ src/GameOver.tsx
@@ -3,7 +3,15 @@ import styled from 'styled-components'
 
 const Text = styled.text`
   text-transform: uppercase;
-  font-size: 15px;
+  font-size: 16px;
+  user-select: none;
+  pointer-events: none;
+`
+const RetryText = styled.text`
+  text-transform: uppercase;
+  font-size: 12px;
+  user-select: none;
+  pointer-events: none;
 `
 
 const GameOver: React.FunctionComponent = () => (
@@ -13,12 +21,18 @@ const GameOver: React.FunctionComponent = () => (
       stroke="#fff"
       x={20 * 1 - 10}
       width={20 * 8 + 20}
-      y={20 * 8}
-      height={20 * 4}
+      y={20 * 8 - 10}
+      height={20 * 5}
     />
-    <Text x={20 * 2 - 10} y={20 * 10 - 10} fill="#fff">
+    <Text x={20 * 2 - 10} y={20 * 9 + 5} fill="#fff">
       Game-over
     </Text>
+    <RetryText x={20 * 2 - 10} y={20 * 11 - 5} fill="#fff" width={20 * 7}>
+      Press 'r' to
+    </RetryText>
+    <RetryText x={20 * 2 - 10} y={20 * 12 - 5} fill="#fff" width={20 * 7}>
+      try again
+    </RetryText>
   </>
 )
 
