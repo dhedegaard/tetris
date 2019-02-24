c5134e8238462219abfac129fa2d8c0ab2e88b15
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-02-24 10:19:37 +0100

    Updates

---
+++ precache-manifest.0fa80b79853c3187f5de3cc924b30533.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "728188aa9ededf2cc535",
+    "url": "/tetris/static/js/main.728188aa.chunk.js"
+  },
+  {
+    "revision": "18d269b309cd4e2e4f13",
+    "url": "/tetris/static/js/runtime~main.18d269b3.js"
+  },
+  {
+    "revision": "65fcc57df90d5c3fa3d2",
+    "url": "/tetris/static/js/2.65fcc57d.chunk.js"
+  },
+  {
+    "revision": "815418ace916ee03ff529d19a59ce268",
+    "url": "/tetris/index.html"
+  }
+];
