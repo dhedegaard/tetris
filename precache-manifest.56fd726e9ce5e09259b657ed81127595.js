0741400d951579271824e1a6c62eeb7a1855b663
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-02-25 21:06:11 +0100

    Updates

--- precache-manifest.56fd726e9ce5e09259b657ed81127595.js
+++ precache-manifest.56fd726e9ce5e09259b657ed81127595.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "2dcccb5e0eb20816cead",
-    "url": "/tetris/static/js/main.2dcccb5e.chunk.js"
+    "revision": "e1579f3ead253b56db11",
+    "url": "/tetris/static/js/main.e1579f3e.chunk.js"
   },
   {
     "revision": "18d269b309cd4e2e4f13",
@@ -12,7 +12,7 @@ self.__precacheManifest = [
     "url": "/tetris/static/js/2.468c7cef.chunk.js"
   },
   {
-    "revision": "338ebdf7c2ce8d3aa6e1c324f897fe4d",
+    "revision": "92dc4c1c0f8667b415d8760c0e284ea4",
     "url": "/tetris/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tetris/precache-manifest.02d492f81f44560e23526ff4ab71d49a.js"
+  "/tetris/precache-manifest.56fd726e9ce5e09259b657ed81127595.js"
 );
 
 workbox.clientsClaim();
