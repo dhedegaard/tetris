f0e106484bbe79d1cd1bc6f243c860cbe46cdd06
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-03-23 20:25:44 +0100

    Extracted the tetris game itself to a separate component from the App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import styled, { createGlobalStyle } from 'styled-components'
-import Block from './components/Block'
-import Grid from './components/Grid'
-import Legend from './components/Legend'
 import Music from './components/Music'
-import { Shape } from './components/shapes'
 import FontLoader from './FontLoader'
-import GameOver from './GameOver'
-import useTetris from './hooks'
+import Game from './Game'
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -23,46 +18,14 @@ const Container = styled.div`
   align-items: center;
 `
 
-const GridContainer = styled.div`
-  margin-top: 10px;
-  border: 4px solid purple;
-`
-
 const App: React.FunctionComponent = () => {
-  const {
-    direction,
-    shape,
-    position,
-    blocks,
-    gamestate,
-    score,
-    peekShapes,
-    level
-  } = useTetris()
-
   return (
     <>
       <GlobalStyle />
       <FontLoader />
       <Music />
       <Container>
-        <GridContainer>
-          <Grid>
-            {gamestate !== 'gameover' && (
-              <Shape
-                direction={direction}
-                shape={shape}
-                x={position.x}
-                y={position.y}
-              />
-            )}
-            {blocks.map(({ x, y }) => (
-              <Block x={x} y={y} key={`block_${x}_${y}`} />
-            ))}
-            {gamestate === 'gameover' && <GameOver />}
-          </Grid>
-        </GridContainer>
-        <Legend score={score} peekShapes={peekShapes} level={level} />
+        <Game />
       </Container>
     </>
   )
---
+++ src/Game.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+import Block from './components/Block'
+import Grid from './components/Grid'
+import Legend from './components/Legend'
+import { Shape } from './components/shapes'
+import GameOver from './GameOver'
+import useTetris from './hooks'
+
+const GridContainer = styled.div`
+  margin-top: 10px;
+  border: 4px solid purple;
+`
+
+const Game: React.FunctionComponent<{}> = () => {
+  const {
+    direction,
+    shape,
+    position,
+    blocks,
+    gamestate,
+    score,
+    peekShapes,
+    level
+  } = useTetris()
+
+  return (
+    <>
+      <GridContainer>
+        <Grid>
+          {gamestate !== 'gameover' && (
+            <Shape
+              direction={direction}
+              shape={shape}
+              x={position.x}
+              y={position.y}
+            />
+          )}
+          {blocks.map(({ x, y }) => (
+            <Block x={x} y={y} key={`block_${x}_${y}`} />
+          ))}
+          {gamestate === 'gameover' && <GameOver />}
+        </Grid>
+      </GridContainer>
+      <Legend score={score} peekShapes={peekShapes} level={level} />
+    </>
+  )
+}
+
+export default Game
