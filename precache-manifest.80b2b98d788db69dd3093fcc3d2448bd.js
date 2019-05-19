4d6ae7b5926a52c8c178d1242be831b5daacd1d7
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-05-19 11:29:18 +0200

    Updates

---
+++ precache-manifest.80b2b98d788db69dd3093fcc3d2448bd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bfc87d788f790db59b84804000cea3a",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "9362aaebdbaea8e6cc75",
+    "url": "/tetris/static/js/2.d36f729d.chunk.js"
+  },
+  {
+    "revision": "2c763580f28a6695cc4d",
+    "url": "/tetris/static/js/main.cbecffdc.chunk.js"
+  },
+  {
+    "revision": "18d269b309cd4e2e4f13",
+    "url": "/tetris/static/js/runtime~main.d015ed9a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tetris/precache-manifest.901901c79f1c2ffd9a1bb6b2f5e44cc5.js"
+  "/tetris/precache-manifest.80b2b98d788db69dd3093fcc3d2448bd.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tetris/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tetris/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
