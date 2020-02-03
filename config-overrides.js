e6c6a7baeef7a2352c6a37f54ecd92b3ac464f58
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-02-03 21:05:46 +0100

    Fix for the service worker handler.

--- config-overrides.js
+++ config-overrides.js
@@ -2,10 +2,12 @@ module.exports = (config, env) => {
   config.plugins.forEach(plugin => {
     if (plugin.constructor.name === 'GenerateSW') {
       plugin.config.skipWaiting = true
-      plugin.config.runtimeCaching = [{
-        urlPattern: /.*/,
-        handler: 'networkFirst',
-      }]
+      plugin.config.runtimeCaching = [
+        {
+          urlPattern: /.*/,
+          handler: 'NetworkFirst'
+        }
+      ]
     }
   })
   return config
