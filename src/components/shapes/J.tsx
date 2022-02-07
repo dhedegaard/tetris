f7619481e33692bcfbef908ed09435fc402c6630
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-02-07 21:34:30 +0100

    linting

--- src/components/shapes/J.tsx
+++ src/components/shapes/J.tsx
@@ -1,37 +1,39 @@
-import { Direction, ShapeProps } from '.'
-import { Coordinates } from '../ShapeDrawer'
+import { Direction, ShapeProps } from ".";
+import { Coordinates } from "../ShapeDrawer";
 
-export const COLOR_J = 'blue'
+export const COLOR_J = "blue";
 
-export default ({ direction, x, y }: ShapeProps): Coordinates => {
+const J = ({ direction, x, y }: ShapeProps): Coordinates => {
   switch (direction) {
     case Direction.UP:
       return [
         { x, y: y - 1 },
         { x, y },
         { x, y: y + 1 },
-        { x: x - 1, y: y + 1 }
-      ]
+        { x: x - 1, y: y + 1 },
+      ];
     case Direction.RIGHT:
       return [
         { x: x - 1, y: y - 1 },
         { x: x - 1, y },
         { x, y },
-        { x: x + 1, y }
-      ]
+        { x: x + 1, y },
+      ];
     case Direction.DOWN:
       return [
         { x, y: y - 1 },
         { x: x + 1, y: y - 1 },
         { x, y },
-        { x, y: y + 1 }
-      ]
+        { x, y: y + 1 },
+      ];
     case Direction.LEFT:
       return [
         { x: x - 1, y },
         { x, y },
         { x: x + 1, y },
-        { x: x + 1, y: y + 1 }
-      ]
+        { x: x + 1, y: y + 1 },
+      ];
   }
-}
+};
+
+export default J;
--- src/components/shapes/T.tsx
+++ src/components/shapes/T.tsx
@@ -1,21 +1,23 @@
-import { Direction, ShapeProps } from '.'
-import { Coordinates } from '../ShapeDrawer'
+import { Direction, ShapeProps } from ".";
+import { Coordinates } from "../ShapeDrawer";
 
-export const COLOR_T = 'purple'
+export const COLOR_T = "purple";
 
-export default ({ direction, x, y }: ShapeProps): Coordinates => {
-  const result: Coordinates = [{ x, y }]
+const T = ({ direction, x, y }: ShapeProps): Coordinates => {
+  const result: Coordinates = [{ x, y }];
   if (direction !== Direction.DOWN) {
-    result.push({ x, y: y - 1 })
+    result.push({ x, y: y - 1 });
   }
   if (direction !== Direction.LEFT) {
-    result.push({ x: x + 1, y })
+    result.push({ x: x + 1, y });
   }
   if (direction !== Direction.UP) {
-    result.push({ x, y: y + 1 })
+    result.push({ x, y: y + 1 });
   }
   if (direction !== Direction.RIGHT) {
-    result.push({ x: x - 1, y })
+    result.push({ x: x - 1, y });
   }
-  return result
-}
+  return result;
+};
+
+export default T;
