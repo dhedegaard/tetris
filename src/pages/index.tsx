6a4cb165911c9b1330422b1387970be87b9d7f5d
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-02-07 21:05:20 +0100

    typing error, packages, etc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,3 @@ import App from "../App";
 import WebFont from "webfontloader";
 
 export default App;
-
-// TODO:
-// WebFont.load({
-//   google: {
-//     families: ["Press Start 2P&display=auto"],
-//   },
-// });
