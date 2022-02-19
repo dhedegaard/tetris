07ca6d894584893d5498ff6308be955c512c16be
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-02-19 14:45:10 +0100

    Fixed displayName for Grid.

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,12 +1,12 @@
 import styled from "@emotion/styled";
 import { SVGProps } from "react";
 
-const Grid = styled((props: SVGProps<SVGSVGElement>) => (
+const Grid = (props: SVGProps<SVGSVGElement>) => (
   <svg viewBox="0 0 10 20" height="100%" {...props} />
-))`
+);
+
+export default styled(Grid)`
   background-color: #000;
   display: block;
   box-sizing: border-box;
 `;
-
-export default Grid;
