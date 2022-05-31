c89efc503a7754bd87fbd984dc8e0290495c564c
Dennis Hedegaard <dennis@dhedegaard.dk>
2022-05-31 21:54:17 +0200

    make tests run again

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
-// jest.config.js
 const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
@@ -8,10 +7,12 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ["node_modules", "<rootDir>/"],
+  moduleNameMapper: {
+    // Handle module aliases (this will be automatically configured for you soon)
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+
+    "^@/pages/(.*)$": "<rootDir>/pages/$1",
+  },
   testEnvironment: "jest-environment-jsdom",
 };
 
