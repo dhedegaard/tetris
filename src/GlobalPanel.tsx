14230abd478f258a1b447c6eb403fa79cab35d19
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-01-28 10:53:20 +0100

    GlobalPanel refactor

--- src/GlobalPanel.tsx
+++ src/GlobalPanel.tsx
@@ -1,91 +1,34 @@
-import styled from "@emotion/styled";
-import { FC } from "react";
-
-const Panel = styled.div`
-  background: #000;
-  height: 100%;
-  aspect-ratio: 120 / 400;
-  box-sizing: border-box;
-  padding: 8px;
-  font-size: 2vh;
-  gap: 8px;
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  overflow-y: auto;
-  overflow-x: hidden;
-
-  @media (max-width: 425px) {
-    display: none;
-  }
-`;
-
-const Container = styled.div`
-  border: 1px double #fff;
-  border-radius: 5px;
-  border-top-left-radius: 2px;
-  border-bottom-right-radius: 2px;
-  padding: 8px;
-`;
-
-const Title = styled.div`
-  color: #fff;
-  border-bottom: 1px solid #fff;
-  margin-bottom: 5px;
-  text-transform: uppercase;
-  padding-bottom: 5px;
-`;
-
-const Text = styled.div`
-  padding-top: 4px;
-  color: #fff;
-  text-transform: uppercase;
-  text-align: right;
-  cursor: pointer;
-`;
-
-const Toggle = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  margin: 0 8px;
-  gap: 8px;
-`;
-
-const H1 = styled.h1`
-  color: #fff;
-  letter-spacing: 4px;
-  text-transform: uppercase;
-  text-decoration: underline;
-  font-size: 3vh;
-  margin: 12px 0;
-  max-width: 100%;
-  width: 100%;
-  text-align: center;
-`;
+import { FC, useCallback } from "react";
+import styles from "./GlobalPanel.module.css";
 
 interface Props {
   musicEnabled: boolean;
   toggleMusic: (enabled: boolean) => void;
 }
 
-const GlobalPanel: FC<Props> = (props) => (
-  <Panel>
-    <H1>Tetris</H1>
-    <Container>
-      <Title>Music:</Title>
-      <Toggle>
-        <Text onClick={() => props.toggleMusic(true)}>
-          {props.musicEnabled ? <>&gt; </> : <>&nbsp; </>}
+const GlobalPanel: FC<Props> = ({ toggleMusic, musicEnabled }) => (
+  <div className={styles.panel}>
+    <h1 className={styles.h1}>Tetris</h1>
+    <div className={styles.container}>
+      <div className={styles.title}>Music:</div>
+      <div className={styles.toggle}>
+        <div
+          className={styles.text}
+          onClick={useCallback(() => toggleMusic(true), [toggleMusic])}
+        >
+          {musicEnabled ? <>&gt; </> : <>&nbsp; </>}
           On
-        </Text>{" "}
-        <Text onClick={() => props.toggleMusic(false)}>
-          {!props.musicEnabled ? <>&gt; </> : <>&nbsp; </>}
+        </div>{" "}
+        <div
+          className={styles.text}
+          onClick={useCallback(() => toggleMusic(false), [toggleMusic])}
+        >
+          {!musicEnabled ? <>&gt; </> : <>&nbsp; </>}
           Off
-        </Text>
-      </Toggle>
-    </Container>
-  </Panel>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default GlobalPanel;
