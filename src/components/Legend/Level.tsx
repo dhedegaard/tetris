c2c5fdce32c2261ccea206b6185c378fd38ae166
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-01-28 10:45:20 +0100

    cleanup

--- src/components/Legend/Level.tsx
+++ src/components/Legend/Level.tsx
@@ -1,29 +1,5 @@
 import { FC, memo, useMemo } from "react";
-import styled from "@emotion/styled";
 import styles from "./Level.module.css";
-
-const Container = styled.div`
-  border: 1px double #fff;
-  border-radius: 5px;
-  border-top-left-radius: 2px;
-  border-bottom-right-radius: 2px;
-  padding: 8px;
-`;
-
-const Title = styled.div`
-  color: #fff;
-  border-bottom: 1px solid #fff;
-  margin-bottom: 5px;
-  text-transform: uppercase;
-  padding-bottom: 5px;
-`;
-
-const Text = styled.div`
-  padding-top: 4px;
-  color: #fff;
-  text-transform: uppercase;
-  text-align: right;
-`;
 
 interface Props {
   level: number;
