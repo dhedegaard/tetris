7f9c67f930457d46888aec4aac3e466fd7dec948
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-01-28 11:20:20 +0100

    cleanup of old safari hacks, for now.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { css, Global } from "@emotion/react";
 import styled from "@emotion/styled";
-import { parse } from "bowser";
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import Music from "./components/Music";
 import { font } from "./font";
 import Game from "./Game";
@@ -22,24 +21,8 @@ const GlobalStyle: FC = () => (
 );
 
 const Container = styled.main<{ $isSafari: boolean }>`
-  box-sizing: border-box;
-  display: flex;
-  height: auto;
   /* Subtract a bit due to macos safari. */
   max-height: calc(100vh - 8px);
-  /* Fix for another height issue in safari on ios. */
-  ${({ $isSafari }) =>
-    $isSafari &&
-    css`
-      max-height: -webkit-fill-available;
-    `}
-  margin: 0 auto;
-  justify-content: center;
-  align-items: stretch;
-  border: 4px solid purple;
-  background-color: purple;
-  gap: 4px;
-  aspect-ratio: 1.11;
 
   @media (max-width: 425px) {
     aspect-ratio: 0.82;
@@ -49,19 +32,11 @@ const Container = styled.main<{ $isSafari: boolean }>`
 const App: FC = () => {
   const [musicEnabled, setMusicEnabled] = useState(false);
 
-  const [isSafari, setIsSafari] = useState(false);
-  useEffect(() => {
-    const browser = parse(window.navigator.userAgent);
-    if (browser?.browser.name === "Safari") {
-      setIsSafari(true);
-    }
-  }, []);
-
   return (
     <>
       <GlobalStyle />
       {musicEnabled && <Music />}
-      <Container $isSafari={isSafari}>
+      <Container className="box-border flex h-auto justify-center items-stretch aspect-[1.11] mx-auto bg-[purple] border-[4px] border-solid border-[purple] gap-[4px]">
         <GlobalPanel
           musicEnabled={musicEnabled}
           toggleMusic={(enabled) => setMusicEnabled(enabled)}
