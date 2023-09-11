92522ff7f6070942d6566c0a9694537d2e8828df
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-09-11 22:02:47 +0200

    fixed music no longer working

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -6,7 +6,7 @@ const Music: FC<{}> = () => (
     className="absolute top-[-20px] right-[-20px]"
     width="10"
     height="10"
-    src="https://www.youtube.com/embed/9Fv5cuYZFC0?controls=0&autoplay=1&loop=1"
+    src="https://www.youtube.com/embed/NmCCQxVBfyM?controls=0&autoplay=1&loop=1"
     frameBorder={0}
     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
   />
