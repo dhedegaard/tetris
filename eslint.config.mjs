8bda147a92e83196c6a49f109860252dc4eac898
Dennis Hedegaard <dennis@dhedegaard.dk>
2025-04-23 14:30:26 +0200

    migrated eslint config to new flat file

---
+++ eslint.config.mjs
@@ -0,0 +1,60 @@
+import { defineConfig } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends(
+        "next/core-web-vitals",
+        "plugin:tailwindcss/recommended",
+        "plugin:@typescript-eslint/strict-type-checked",
+    ),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            project: true,
+        },
+    },
+
+    rules: {
+        "no-console": ["error", {
+            allow: ["warn", "error"],
+        }],
+
+        eqeqeq: ["error", "smart"],
+
+        "@typescript-eslint/no-misused-promises": ["error", {
+            checksVoidReturn: {
+                attributes: false,
+            },
+        }],
+
+        "@typescript-eslint/strict-boolean-expressions": "error",
+        "prefer-const": "error",
+
+        "@typescript-eslint/restrict-template-expressions": ["error", {
+            allowBoolean: true,
+            allowNumber: true,
+            allowNullish: true,
+        }],
+    },
+}]);
