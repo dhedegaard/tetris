ba15e71adb44543b506b06dd47b5fc42f1ea947a
Dennis Hedegaard <dennis@dhedegaard.dk>
2025-04-23 13:33:23 +0200

    avoid appending the renderType to the DOM node

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,5 +1,7 @@
 import { memo, SVGProps } from 'react'
 import styles from './Block.module.css'
+import clsx from 'clsx'
+import { match } from 'ts-pattern'
 
 export interface BlockProps extends SVGProps<SVGRectElement> {
   renderType: 'ghost' | 'normal'
@@ -7,14 +9,20 @@ export interface BlockProps extends SVGProps<SVGRectElement> {
   y: number
 }
 
-export const Block = memo<BlockProps>(function Block(props: BlockProps) {
+export const Block = memo<BlockProps>(function Block({ renderType, ...props }) {
   return (
     <rect
       {...props}
       width={1}
       height={1}
-      // TODO: Change the stroke type for ghost rendering.
-      className={`stroke-black stroke-[0.02] text-inherit ${styles['block'] as string}`}
+      className={clsx(
+        styles['block'] as string,
+        'stroke-[0.025] text-inherit',
+        match(renderType)
+          .with('ghost', () => 'opacity-40')
+          .with('normal', () => 'stroke-black')
+          .exhaustive()
+      )}
     />
   )
 })
