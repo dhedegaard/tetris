7fcf554e31068bc7575559fe9e5b7d499ebb7a9e
Dennis Hedegaard <dennis@dhedegaard.dk>
2025-04-23 14:46:51 +0200

    avoid using lodash-es omit for removing a prop, that should not be exposed in the DOM

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx'
-import { omit } from 'lodash-es'
-import { SVGProps } from 'react'
+import { memo, SVGProps, useMemo } from 'react'
 import styles from './Block.module.css'
 
 export interface BlockProps extends SVGProps<SVGRectElement> {
@@ -9,13 +8,15 @@ export interface BlockProps extends SVGProps<SVGRectElement> {
   y: number
 }
 
-export const Block = ({ x, y, ...props }: BlockProps) => (
-  <rect
-    {...omit(props, 'renderType')}
-    x={x}
-    y={y}
-    width={1}
-    height={1}
-    className={clsx(styles['block'] as string, 'stroke-black stroke-[0.025] text-inherit')}
-  />
-)
+export const Block = memo(function Block({ x, y, ...props }: BlockProps) {
+  return (
+    <rect
+      {...useMemo(() => ({ ...props, renderType: undefined }), [props])}
+      x={x}
+      y={y}
+      width={1}
+      height={1}
+      className={clsx(styles['block'] as string, 'stroke-black stroke-[0.025] text-inherit')}
+    />
+  )
+})
