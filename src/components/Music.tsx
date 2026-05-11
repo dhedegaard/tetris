0a8c70f49fb259c273c152b64a1306a6afd1fb30
Dennis Hedegaard <dennis@dhedegaard.dk>
2026-05-11 22:00:38 +0200

    make sure the music loops

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -7,7 +7,7 @@ export const Music = memo(function Music() {
       className="absolute top-[-20px] right-[-20px]"
       width="10"
       height="10"
-      src="https://www.youtube.com/embed/NmCCQxVBfyM?controls=0&autoplay=1&loop=1"
+      src="https://www.youtube.com/embed/NmCCQxVBfyM?controls=0&autoplay=1&loop=1&playlist=NmCCQxVBfyM"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
     />
   )
